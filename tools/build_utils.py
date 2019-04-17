cd351b0a6f2ec1e641f69eb3eadfdf7cba39fcbc
shresthamalik <shrestha.malik@intel.com>
2019-04-16 18:02:25 -0700

    formatting change

--- tools/build_utils.py
+++ tools/build_utils.py
@@ -145,9 +145,9 @@ def setup_venv(venv_dir):
         "termcolor>=1.1.0",
         "protobuf>=3.6.1",
         "keras_applications>=1.0.6",
-        "--no-deps --no-cache-dir",
+        "--no-deps",
         "keras_preprocessing==1.0.5",
-        "--no-deps --no-cache-dir",
+        "--no-deps",
         "yapf==0.26.0",
     ]
     command_executor(package_list)
