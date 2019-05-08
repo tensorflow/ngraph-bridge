f09d7b4b60ae4e62c247fd6e5111164a7a9f4656
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-05-08 00:19:16 -0700

    Python UT

---
+++ test/python/test_cnms.py
@@ -0,0 +1,54 @@
+# ==============================================================================
+#  Copyright 2019 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ==============================================================================
+"""nGraph TensorFlow bridge floor operation test
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pytest
+
+import tensorflow as tf
+import numpy as np
+from common import NgraphTest
+from google.protobuf import text_format
+
+class TestFloorOperations(NgraphTest):
+
+    # TODO disable this test
+    #@pytest.mark.skip(reason="Backend specific test")
+    def test_cmns(self):
+        graph_def = tf.GraphDef()
+        with open('cnms.pbtxt', "r") as f:
+            text_format.Merge(f.read(), graph_def)
+
+        with tf.Graph().as_default() as g:
+            tf.import_graph_def(graph_def)
+            #import pdb; pdb.set_trace()
+            input_placeholders = [g.get_tensor_by_name("import/arg_" + str(i) + ':0') for i in range(6)]
+
+            outputs = [g.get_tensor_by_name("import/CombinedNonMaxSuppression:" + str(i)) for i in range(4)]
+
+            sess_fn = lambda sess: sess.run(
+                outputs, feed_dict={input_placeholders[0]: 19*np.random.random([10, 2, 1, 4]) + 1,
+                input_placeholders[1]: np.random.random([10, 2, 20]),
+                input_placeholders[2]: 3,
+                input_placeholders[3]: 4,
+                input_placeholders[4]: 0.2,
+                input_placeholders[5]: 0.3,
+                })
+            assert np.isclose(self.with_ngraph(sess_fn), self.without_ngraph(sess_fn)).all()
+            
