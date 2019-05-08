df07e421458f4fafa8d11c3a9b8d959a249a3137
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-05-08 01:04:11 -0700

    Correcting pytest to remove randomness

--- test/python/test_cnms.py
+++ test/python/test_cnms.py
@@ -26,6 +26,7 @@ import numpy as np
 from common import NgraphTest
 from google.protobuf import text_format
 
+
 class TestFloorOperations(NgraphTest):
 
     # TODO disable this test
@@ -37,23 +38,29 @@ class TestFloorOperations(NgraphTest):
 
         with tf.Graph().as_default() as g:
             tf.import_graph_def(graph_def)
-            input_placeholders = [g.get_tensor_by_name("import/arg_" + str(i) + ':0') for i in range(6)]
+            input_placeholders = [
+                g.get_tensor_by_name("import/arg_" + str(i) + ':0')
+                for i in range(6)
+            ]
 
-            outputs = [g.get_tensor_by_name("import/CombinedNonMaxSuppression:" + str(i)) for i in range(4)]
-
+            outputs = [
+                g.get_tensor_by_name("import/CombinedNonMaxSuppression:" +
+                                     str(i)) for i in range(4)
+            ]
+            boxes = 19 * np.random.random([10, 2, 1, 4]) + 1
+            scores = np.random.random([10, 2, 20])
             sess_fn = lambda sess: sess.run(
-                outputs, feed_dict={input_placeholders[0]: 19*np.random.random([10, 2, 1, 4]) + 1,
-                input_placeholders[1]: np.random.random([10, 2, 20]),
-                input_placeholders[2]: 3,
-                input_placeholders[3]: 4,
-                input_placeholders[4]: 0.2,
-                input_placeholders[5]: 0.3,
+                outputs,
+                feed_dict={
+                    input_placeholders[0]: boxes,
+                    input_placeholders[1]: scores,
+                    input_placeholders[2]: 3,
+                    input_placeholders[3]: 4,
+                    input_placeholders[4]: 0.2,
+                    input_placeholders[5]: 0.3,
                 })
 
-            #assert np.isclose(self.with_ngraph(sess_fn), self.without_ngraph(sess_fn)).all()
-
-            for res1, res2 in zip(self.with_ngraph(sess_fn), self.without_ngraph(sess_fn)):
-                #assert np.isclose(res1 ,res2).all()
-                # TODO: currently verifyimg shape, but should verify values
-                assert np.isclose(res1.shape ,res2.shape).all()
-            
+            for res1, res2 in zip(
+                    self.with_ngraph(sess_fn), self.without_ngraph(sess_fn)):
+                assert np.isclose(res1.shape, res2.shape).all()
+                assert np.isclose(res1, res2).all()
