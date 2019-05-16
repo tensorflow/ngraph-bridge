7459cf8fe3f4c6e407c543350e11c357dbf331c1
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-05-16 10:56:07 -0700

    Update common.py

--- test/python/common.py
+++ test/python/common.py
@@ -34,20 +34,13 @@ LIBNGRAPH_BRIDGE = 'libngraph_bridge.' + _ext
 
 class NgraphTest(object):
 
-    @staticmethod
-    def get_tensor(graph, tname, loading_from_protobuf):
-        return graph.get_tensor_by_name(("", "import/")[loading_from_protobuf] +
-                                        tname)
+    def get_tensor(self, graph, tname):
+        return graph.get_tensor_by_name("import/" + tname)
 
-    @staticmethod
-    def import_protobuf(pb_filename):
+    def import_pbtxt(self, pb_filename):
         graph_def = tf.GraphDef()
-        if pb_filename.endswith("pbtxt"):
-            with open(pb_filename, "r") as f:
-                text_format.Merge(f.read(), graph_def)
-        else:
-            with open(pb_filename, "rb") as f:
-                graph_def.ParseFromString(f.read())
+        with open(pb_filename, "r") as f:
+            text_format.Merge(f.read(), graph_def)
 
         with tf.Graph().as_default() as graph:
             tf.import_graph_def(graph_def)
