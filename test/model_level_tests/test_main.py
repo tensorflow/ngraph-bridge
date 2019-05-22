69abb07411ec0813c6a401ad7d11adfbce868f26
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-05-22 12:01:37 -0700

    Add provision for cleanup script

--- test/model_level_tests/test_main.py
+++ test/model_level_tests/test_main.py
@@ -24,11 +24,11 @@ from build_utils import download_repo
 from tf2ngraph import get_gdef
 import atexit
 
-#TODO: update ngraph_enable.patch to be compatible with grappler build
+# TODO: update ngraph_enable.patch to be compatible with grappler build
 
 
 def parse_json(json_file_name):
-    #TODO: check if the json is in agreed-upon format and has all relevant info
+    # TODO: check if the json is in agreed-upon format and has all relevant info
     with open(json_file_name) as f:
         return json.load(f)
 
@@ -102,13 +102,14 @@ def apply_patch_and_test(test_folder):
 @return_to_cwd
 def ready_repo(model_dir, repo_dl_loc):
     command_executor('cd ' + repo_dl_loc)
+    command_executor('git reset --hard')
     # getting the repo ready is common to both check_rewrite_test and get_checkpoint
     if os.path.isfile(model_dir + '/getting_repo_ready.sh'):
         command_executor(model_dir + '/getting_repo_ready.sh', verbose=True)
 
 
 def rewrite_test(model_dir):
-    #TODO: assert TF version. Some models may not run on TF1.12 etc
+    # TODO: assert TF version. Some models may not run on TF1.12 etc
     model_dir = os.path.abspath(model_dir)
 
     repo_filename = model_dir + '/repo.txt'
@@ -121,7 +122,7 @@ def rewrite_test(model_dir):
         repo_name = repo_info[0]
         repo_version = repo_info[1] if len(repo_info) == 2 else 'master'
         repo_dl_loc = model_dir + '/downloaded_model'
-        #TODO: download only when needed?
+        # TODO: download only when needed?
         download_repo(repo_dl_loc, repo_name, repo_version)
 
         ready_repo(model_dir, repo_dl_loc)
@@ -155,14 +156,22 @@ def rewrite_test(model_dir):
             assert False, "Unknown input format. Expected savedmodel, pb or pbtxt"
         gdef = get_gdef(model_format, model_dir + '/' + model)
 
+    cleanup_script = model_dir + '/cleanup.sh'
+    #pdb.set_trace()
+    if os.path.isfile(cleanup_script):
+        command_executor(cleanup_script)
+    command_executor.commands += '# Exiting. Done with tests in ' + model_dir.split('/')[-1]
+    # TODO: delete downloaded model repo
+
         # TODO: use gdef to run
         # TODO: add axpy test folders for pb. pbtxt and savedmodel
+        # TODO integrate the if-else paths as much as possible
 
-    #TODO: check if failed or passed
+    # TODO: check if failed or passed
     # TODO: check if ran to completion
     # TODO: check if ran within a prefixed amount of time
     # TODO: check throughput/latency
-    #TODO: check if atleast some stuff was placed on ngraph. Leverage LOG_PLACEMENT
+    # TODO: check if atleast some stuff was placed on ngraph. Leverage LOG_PLACEMENT
 
 
 def get_checkpoint():
@@ -235,6 +244,13 @@ if __name__ == '__main__':
         help=
         'List all tests if empty string is passed, else list tests of the directories in the comma separated string that was passed',
         default=None)
+    parser.add_argument(
+        '--configuration',
+        action='store',
+        type=str,
+        help=
+        "The configuration in which the test is run (to choose which expected values current run's results will be compared against)",
+        default='default')
 
     # This script must be run from this location
     assert cwd.split('/')[-1] == 'model_level_tests'
@@ -264,11 +280,12 @@ if __name__ == '__main__':
         if args.functional:
             print('Functional tests not implemented yet!!')
 
-#TODO verbose or quiet?
-#TODO: output a shell script, for debugging purposes
+# TODO verbose or quiet?
+# TODO: output a shell script, for debugging purposes
+# TODO: add a way to disable tests and subtests through the CLI
 
-#TODO: we have a way to control which model/test-dirs run (using --models). But we do not have a flag for test "intensity".
-#each subtest folder has a "enable" patch and a run command.
+# TODO: we have a way to control which model/test-dirs run (using --models). But we do not have a flag for test "intensity".
+# each subtest folder has a "enable" patch and a run command.
 # Level1: These are run with "parse the NGRAPH_TF_LOG_PLACEMENT=1". These tests should be short
 # Level2: Dump pbtxts and run verify models (needs an input file that specifies certain layers. (what about all layers?)). These tests should be short
 # When Level1 is running, dump out pbtxts that can be used for Level2 tests
@@ -276,14 +293,18 @@ if __name__ == '__main__':
 # These tests can be long
 # So we can offer options to do: {1}, {1,2}, {1,2,3}, {3}  (or do we allow options for any combination of tests?)
 
+# Each model dir represents 1 repo to download. A model dir can have multiple sub tests (each sub-test could represent a different model, or the same model tested under different settings)
+
 # Structure of "expected json"
 # dictionary of expected values. key is a config, value is the expected values json. there is a "default" config, but one can add other configs (for example for other backends etc)
 
 
-#TODO: update main README.md. Document "how-to-use" and features
+# TODO: update main README.md. Document "how-to-use" and features
 
 # Sample run script:
 # python test_main.py --rewrite_test --models MLP
 
 # feature 1: dumps shell script at the end. dumps shell script even when the framework crashes
 # feature 2: prints list of tests and their descriptions (--list)
+# feature 3: "expected" values can be varied by different configs
+# feature 4: cleanup script
