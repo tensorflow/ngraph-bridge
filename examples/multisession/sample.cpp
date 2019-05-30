011e4b3df69f2602acf5feec242b12423c2b252c
avijit-nervana <avijit.chakraborty@intel.com>
2019-05-30 11:02:42 -0700

    WIP

--- examples/multisession/sample.cpp
+++ examples/multisession/sample.cpp
@@ -3,38 +3,78 @@
 #include <vector>
 
 #include "tensorflow/core/framework/graph.pb.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/tensor_types.h"
+#include "tensorflow/core/graph/algorithm.h"
+#include "tensorflow/core/graph/default_device.h"
+#include "tensorflow/core/graph/graph.h"
+#include "tensorflow/core/graph/graph_constructor.h"
+#include "tensorflow/core/platform/env.h"
+
+#include "tensorflow/cc/client/client_session.h"
+#include "tensorflow/cc/ops/standard_ops.h"
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/public/session.h"
+
+#include "ngraph_backend_manager.h"
 
 using namespace tensorflow;
 
 int main(int argc, char** argv) {
   // Construct your graph.
-  tensorflow::GraphDef graph;  // = ...;
+  tensorflow::Scope root = tensorflow::Scope::NewRootScope();
 
-  // Create a Session running TensorFlow locally in process.
-  std::unique_ptr<tensorflow::Session> session(tensorflow::NewSession({}));
+  // Matrix A = [3 2; -1 0]
+  auto A = tensorflow::ops::Const(root, {{0.03f, 0.022f}, {-0.001f, 0.025f}});
+  // Vector b = [3 5]
+  auto b = tensorflow::ops::Const(root, {{0.345f, 0.35f}});
+  // v = Ab^T
+  auto v = tensorflow::ops::MatMul(root.WithOpName("v"), A, b,
+                                   tensorflow::ops::MatMul::TransposeB(true));
+  // R = softmax(v)
+  auto R = tensorflow::ops::Softmax(root, v);
 
-  // Initialize the session with the graph.
-  tensorflow::Status s = session->Create(graph);
-  // if (!s.ok()) { ... }
+  // Turn off optimizations so that all the nodes are processed
+  tensorflow::SessionOptions options;
+  options.config.mutable_graph_options()
+      ->mutable_optimizer_options()
+      ->set_opt_level(tensorflow::OptimizerOptions_Level_L0);
+  options.config.mutable_graph_options()
+      ->mutable_rewrite_options()
+      ->set_constant_folding(tensorflow::RewriterConfig::OFF);
 
-  // Specify the 'feeds' of your network if needed.
-  std::vector<std::pair<string, tensorflow::Tensor>> inputs;
+  options.config.mutable_graph_options()
+      ->mutable_rewrite_options()
+      ->add_custom_optimizers()
+      ->set_name("ngraph-optimizer");
 
-  // Run the session, asking for the first output of "my_output".
+  options.config.mutable_graph_options()
+    ->mutable_rewrite_options()->set_min_graph_nodes(-1);
+
+  auto supported_backends =
+      tensorflow::ngraph_bridge::BackendManager::GetSupportedBackendNames();
+  vector<string> backends(supported_backends.begin(), supported_backends.end());
+
+  cout << "Available backends: " << endl;
+  for (auto& backend_name : backends) {
+    cout << "Backend: " << backend_name << std::endl;
+  }
+
+  std::cout
+      << "Currently selected backend: "
+      << tensorflow::ngraph_bridge::BackendManager::GetCurrentlySetBackendName()
+      << std::endl;
+
+  tensorflow::ClientSession session(root, options);
+
   std::vector<tensorflow::Tensor> outputs;
-  s = session->Run(inputs, {"my_output:0"}, {}, &outputs);
-// f (!s.ok()) { ... }
+  session.Run({R}, &outputs);
 
-// Do something with your outputs
-#if 0
-  auto output_vector = outputs[0].vec<float>();
-  if (output_vector(0) > 0.5) { ... }
-#endif
+  // Print the output
+  std::cout << "Result: " << outputs[0].matrix<float>() << std::endl;
 
   // Close the session.
-  session->Close();
+  //session.Close();
 
   return 0;
 }
