fa90d5c4f7f4efeaa5240076c18707127774acab
Shrestha Malik <shrestha.malik@intel.com>
2019-06-10 14:52:26 -0700

    Fixed a compile issue

--- src/enable_variable_ops/ngraph_rewrite_pass.cc
+++ src/enable_variable_ops/ngraph_rewrite_pass.cc
@@ -298,7 +298,7 @@ class NGraphEncapsulationPass : public NGraphRewritePass {
     // 4. Encapsulate clusters then, if requested, dump the graphs.
     FunctionDefLibrary* fdeflib_new = new FunctionDefLibrary();
     TF_RETURN_IF_ERROR(
-        EncapsulateClusters(options.graph->get(), idx, fdeflib_new, config_map);
+        EncapsulateClusters(options.graph->get(), idx, fdeflib_new, config_map));
     // TODO: not using fdeflib_new in this path. Only grappler path uses it
     free(fdeflib_new);
     if (DumpEncapsulatedGraphs()) {
