0607bd43e4186b7929c119fea8965257c881d919
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-06-14 14:52:57 -0700

    Upgrade to tf114: ngraph-models

--- test/model_level_tests/models/ngraph-models/test1/custom_log_parser.py
+++ test/model_level_tests/models/ngraph-models/test1/custom_log_parser.py
@@ -1,6 +1,6 @@
 def custom_parse_logs(log_lines):
     outlines = log_lines.split('\n')
     test_line_idx = [idx for idx, line in enumerate(outlines) if line.startswith('TEST: ')]
-    return {outlines[i]:str(outlines[i+1]=='PASSED') for i in test_line_idx}
+    return {"0":{outlines[i]:str(outlines[i+1]=='PASSED') for i in test_line_idx}}
 
 
