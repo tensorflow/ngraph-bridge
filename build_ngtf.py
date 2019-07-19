e8a15b20f4137ecc11a2ff5b5ab5bd36e042d227
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-07-19 09:41:39 -0400

    add arguments in start_container

--- build_ngtf.py
+++ build_ngtf.py
@@ -195,7 +195,7 @@ def main():
     if arguments.run_in_docker:
         if check_container() == True:
             stop_container(arguments)
-        start_container("/ngtf")
+        start_container("/ngtf", arguments)
         run_in_docker("/ngtf/build_ngtf.py", arguments)
         return
 
