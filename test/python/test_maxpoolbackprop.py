af960eaf67a2b3c730bb207fa2d7d3c5b6a6b6f6
Kanvi <kanvi.khanna@intel.com>
2019-08-02 16:32:20 -0700

    Changes for unskipping the maxpoolbackprop py test

--- test/python/test_maxpoolbackprop.py
+++ test/python/test_maxpoolbackprop.py
@@ -32,7 +32,6 @@ NHWC_TO_NCHW = (0, 3, 1, 2)
 NCHW_TO_NHWC = (0, 2, 3, 1)
 
 
-@pytest.mark.skip(reason="new deviceless mode WIP")
 class TestMaxPoolBackpropInput(NgraphTest):
     input_nhwc = np.random.rand(128, 224, 224, 3)
     input_nchw = np.transpose(input_nhwc, NHWC_TO_NCHW)
@@ -59,32 +58,10 @@ class TestMaxPoolBackpropInput(NgraphTest):
             grad = tf.placeholder(tf.float32, shape=(128, 112, 74, 3))
         elif padding == "SAME":
             grad = tf.placeholder(tf.float32, shape=(128, 112, 75, 3))
+        out = max_pool_grad( self.input_nhwc, output, grad, ksize, strides, padding=padding, data_format="NHWC")
+        sess_fn = lambda sess: sess.run(out, feed_dict={grad: np_nhwc})
+        assert (np.allclose(self.with_ngraph(sess_fn), self.without_ngraph(sess_fn), rtol=5e-7))
 
-        with self.device:
-            a = max_pool_grad(
-                self.input_nhwc,
-                output,
-                grad,
-                ksize,
-                strides,
-                padding=padding,
-                data_format="NHWC")
-            with self.session as sess:
-                result = sess.run(a, feed_dict={grad: np_nhwc})
-
-        with tf.device('/cpu:0'):
-            b = max_pool_grad(
-                self.input_nhwc,
-                output,
-                grad,
-                ksize,
-                strides,
-                padding=padding,
-                data_format="NHWC")
-            with self.session as sess:
-                expected = sess.run(b, feed_dict={grad: np_nhwc})
-
-        np.testing.assert_allclose(result, expected, rtol=5e-7)
 
     @pytest.mark.parametrize("padding", ("VALID", "SAME"))
     def test_nchw(self, padding):
@@ -97,7 +74,7 @@ class TestMaxPoolBackpropInput(NgraphTest):
         elif padding == "SAME":
             grad = tf.placeholder(tf.float32, shape=(128, 3, 112, 75))
 
-        with self.device:
+        def test_on_ng(sess):
             a = max_pool_grad(
                 self.input_nchw,
                 output,
@@ -106,12 +83,12 @@ class TestMaxPoolBackpropInput(NgraphTest):
                 strides,
                 padding=padding,
                 data_format="NCHW")
-            with self.session as sess:
-                result = sess.run(a, feed_dict={grad: np_nchw})
+            return sess.run(a, feed_dict={grad: np_nchw})
+
         # To validate on the CPU side we will need to run in NHWC, because the CPU
-        # implementation of avgpool backprop does not support NCHW. We will
+        # implementation of maxpool backprop does not support NCHW. We will
         # transpose on the way in and on the way out
-        with tf.device('/cpu:0'):
+        def test_on_tf(sess):
             grad = tf.transpose(grad, NCHW_TO_NHWC)
             np_nhwc = self.grad_nhwc[padding]
             output = self.output_nhwc
@@ -126,7 +103,7 @@ class TestMaxPoolBackpropInput(NgraphTest):
                 padding=padding,
                 data_format="NHWC")
             b = tf.transpose(b, NHWC_TO_NCHW)
-            with self.session as sess:
-                expected = sess.run(b, feed_dict={grad: np_nhwc})
+            return sess.run(b, feed_dict={grad: np_nhwc})
 
-        np.testing.assert_allclose(result, expected, rtol=5e-7)
+        assert np.allclose(
+            self.with_ngraph(test_on_ng), self.without_ngraph(test_on_tf))
