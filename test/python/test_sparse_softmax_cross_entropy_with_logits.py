fa7ec18d3887a2dde71ed53a756f3c1cd7445dee
Kanvi <kanvi.khanna@intel.com>
2019-08-05 12:16:52 -0700

    Add random seed and precision for softmax test

--- test/python/test_sparse_softmax_cross_entropy_with_logits.py
+++ test/python/test_sparse_softmax_cross_entropy_with_logits.py
@@ -28,6 +28,7 @@ from tensorflow.python.ops.gen_nn_ops import sparse_softmax_cross_entropy_with_l
 import numpy as np
 from common import NgraphTest
 
+np.random.seed(5)
 
 class TestSparseSoftmaxCrossEntropyWithLogitsOperations(NgraphTest):
 
@@ -49,5 +50,5 @@ class TestSparseSoftmaxCrossEntropyWithLogitsOperations(NgraphTest):
         expected = self.without_ngraph(sess_fn)
         result = self.with_ngraph(sess_fn)
 
-        assert np.allclose(result[0], expected[0])
-        assert np.allclose(result[1], expected[1])
+        assert np.allclose(result[0], expected[0], rtol=0, atol=1e-02)
+        assert np.allclose(result[1], expected[1], rtol=0, atol=1e-02)
