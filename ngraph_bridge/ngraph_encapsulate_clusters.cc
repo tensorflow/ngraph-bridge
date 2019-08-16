2b724bfaaf8a931b63a41fa40ead64cef73ec2aa
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-08-16 15:56:04 -0700

    Minor fix

--- ngraph_bridge/ngraph_encapsulate_clusters.cc
+++ ngraph_bridge/ngraph_encapsulate_clusters.cc
@@ -769,7 +769,7 @@ Status EncapsulateClusters(
               // For e.g. _ngraph_ice_cores --> ice_cores
               if (itr.first.find("_ngraph_") != std::string::npos) {
                 // leave out _ngraph_aot_requested
-                if (attr_name.find("_ngraph_aot_requested") != std::string::npos){
+                if (itr.first.find("_ngraph_aot_requested") != std::string::npos){
                   additional_attribute_map.insert(
                       {itr.first.substr(strlen("_ngraph_")), itr.second.s()});
                 }
