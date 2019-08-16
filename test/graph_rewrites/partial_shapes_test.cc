3dbe7354fcc7b33220edbb607ca8d61b6c71d405
Kanvi Khanna <kanvi.khanna@intel.com>
2019-08-16 15:52:12 -0700

    Simple initial test

---
+++ test/graph_rewrites/partial_shapes_test.cc
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright 2019 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+#include "gtest/gtest.h"
+
+#include "tensorflow/core/graph/graph.h"
+#include "tensorflow/core/graph/node_builder.h"
+
+#include "logging/tf_graph_writer.h"
+#include "ngraph_bridge/ngraph_partial_shapes.h"
+#include "ngraph_bridge/ngraph_utils.h"
+#include "test/test_utilities.h"
+
+using namespace std;
+namespace ng = ngraph;
+
+namespace tensorflow {
+
+namespace ngraph_bridge {
+
+namespace testing {
+
+#define ASSERT_OK(x) ASSERT_EQ((x), ::tensorflow::Status::OK());
+#define ASSERT_NOT_OK(x) ASSERT_NE((x), ::tensorflow::Status::OK());
+
+TEST(PartialShapes, Concretize) {
+    PartialShape p1({2,-1});
+    PartialShape p2({-1,3});
+    cout << p1.is_valid() << "\n";
+    p1.concretize(p2);
+    bool result = p1.is_valid();
+    cout << "result " << result;
+
+}
+
+}  // namespace testing
+
+}  // namespace ngraph_bridge
+
+}  // namespace tensorflow
