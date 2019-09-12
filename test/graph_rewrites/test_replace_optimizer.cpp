8f030e1740280eaa1d1733a96e6247f741bb2680
Vishakha Agrawal <vishakha.agrawal@intel.com>
2019-09-11 18:06:01 -0700

    Apply code format

--- test/graph_rewrites/test_replace_optimizer.cpp
+++ test/graph_rewrites/test_replace_optimizer.cpp
@@ -19,14 +19,14 @@
 #include "tensorflow/core/framework/tensor.h"
 #include "tensorflow/core/public/session.h"
 
-#include "ngraph_bridge/enable_variable_ops/ngraph_replace_op_utilities.h"
-#include "ngraph_bridge/enable_variable_ops/ngraph_replace_variable_modifiers.h"
 #include "gtest/gtest.h"
 #include "logging/tf_graph_writer.h"
+#include "ngraph_bridge/enable_variable_ops/ngraph_replace_op_utilities.h"
+#include "ngraph_bridge/enable_variable_ops/ngraph_replace_variable_modifiers.h"
 #include "ngraph_bridge/ngraph_assign_clusters.h"
 #include "ngraph_bridge/ngraph_backend_manager.h"
-#include "ngraph_bridge/ngraph_mark_for_clustering.h"
 #include "ngraph_bridge/ngraph_capture_variables.h"
+#include "ngraph_bridge/ngraph_mark_for_clustering.h"
 #include "test/test_utilities.h"
 
 using namespace std;
