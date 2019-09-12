9cbba4fdfcb6e5bc570613a1334c999e2678eeb7
Vishakha Agrawal <vishakha.agrawal@intel.com>
2019-09-11 18:02:06 -0700

    Update test/graph_rewrites/test_replace_optimizer.cpp
    Co-Authored-By: kanvi-nervana <kanvi.khanna@intel.com>

--- test/graph_rewrites/test_replace_optimizer.cpp
+++ test/graph_rewrites/test_replace_optimizer.cpp
@@ -26,7 +26,7 @@
 #include "ngraph_bridge/ngraph_assign_clusters.h"
 #include "ngraph_bridge/ngraph_backend_manager.h"
 #include "ngraph_bridge/ngraph_mark_for_clustering.h"
-#include "ngraph_capture_variables.h"
+#include "ngraph_bridge/ngraph_capture_variables.h"
 #include "test/test_utilities.h"
 
 using namespace std;
