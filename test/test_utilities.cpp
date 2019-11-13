37f4858188c464dd27dd9816b5942a0e9c208345
Kanvi Khanna <kanvi.khanna@intel.com>
2019-11-13 14:33:58 -0800

    format

--- test/test_utilities.cpp
+++ test/test_utilities.cpp
@@ -160,7 +160,7 @@ std::vector<string> ConvertToString(const std::vector<tensorflow::Tensor> T1) {
   std::vector<string> out;
   for (auto i = 0; i < T1.size(); i++) {
     int total_enteries = 1;
-    for (int j=0; j<T1[i].dims(); j++) {
+    for (int j = 0; j < T1[i].dims(); j++) {
       total_enteries = total_enteries * T1[i].dim_size(j);
     }
     out.push_back(T1[i].SummarizeValue(total_enteries));
