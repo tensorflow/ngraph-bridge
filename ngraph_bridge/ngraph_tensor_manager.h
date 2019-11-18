91c45798d91263085c19fbd0cc32ee710d7ff088
Shrestha Malik <shrestha.malik@intel.com>
2019-11-18 11:10:27 -0800

    Compiles

--- ngraph_bridge/ngraph_tensor_manager.h
+++ ngraph_bridge/ngraph_tensor_manager.h
@@ -13,3 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
+
+#ifndef NGRAPH_TF_TENSOR_MANAGER_H_
+#define NGRAPH_TF_TENSOR_MANAGER_CC_
+#pragma once
+
+#include <mutex>
+#include <ostream>
+#include <vector>
+
+#include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/graph/graph.h"
+
+#include "ngraph/ngraph.hpp"
+
+#include "logging/ngraph_log.h"
+#include "ngraph_bridge/ngraph_freshness_tracker.h"
+#include "ngraph_bridge/ngraph_pipelined_tensors.h"
+
+namespace tensorflow {
+
+namespace ngraph_bridge {
+
+class NGraphTensorManager {
+ public:
+  explicit NGraphTensorManager(const int number_of_inputs,
+                               const int number_of_outputs);
+
+  ~NGraphTensorManager();
+
+ private:
+  int m_number_of_inputs;
+  int m_number_of_outputs;
+};
+
+}  // namespace ngraph_bridge
+
+}  // namespace tensorflow
+#endif  // NGRAPH_TF_TENSOR_MANAGER_H_
