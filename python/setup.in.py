ca879a66a69468fa0911cb7f52dd5e36464b1d40
Sayantan Sarkar <sayantan.sarkar@intel.com>
2019-12-05 13:06:10 -0800

    Revert "Vishakha/cumsum (#364)"
    This reverts commit bec0d8ac55fb68eb183fbc95d8ec5fb262e95dc9.

--- python/setup.in.py
+++ python/setup.in.py
@@ -59,7 +59,7 @@ package_data_dict['ngraph_bridge'] = include_list
 
 setup( 
     name='ngraph_tensorflow_bridge',
-    version='0.19.0rc7',
+    version='0.19.0rc6',
     description='Intel nGraph compiler and runtime for TensorFlow',
     long_description=long_description,
     long_description_content_type="text/markdown",
