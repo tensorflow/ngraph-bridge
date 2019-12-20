bf3b84664a35ec43aa5b5e5d52ccffbbee937728
Shrestha Malik <shrestha.malik@intel.com>
2019-12-19 17:00:18 -0800

    fix test_utils.py

--- tools/test_utils.py
+++ tools/test_utils.py
@@ -130,7 +130,7 @@ def run_ngtf_pytests(venv_dir, build_dir):
         build_dir) + " --ignore=" + build_dir + "/test/python/bfloat16"
     env = os.environ.copy()
     new_paths = venv_dir + '/bin/python3:' + os.path.abspath(
-        build_dir) + ":" + os.path.abspath(axpy_dir) + os.path.abspath(
+        build_dir) + ":" + os.path.abspath(axpy_dir) + ":" + os.path.abspath(
             mnist_dir)
     if 'PYTHONPATH' in env:
         env["PYTHONPATH"] = new_paths + ":" + env["PYTHONPATH"]
