d8ca5878b2890a5a84b176b32a1e6fdcae261c37
Shrestha Malik <shrestha.malik@intel.com>
2020-01-07 15:12:59 -0800

    updated ngraph version

--- build_ngtf.py
+++ build_ngtf.py
@@ -53,7 +53,7 @@ def main():
     '''
 
     # Component versions
-    ngraph_version = "r0.27.1"
+    ngraph_version = "33ca594a1856179037d70633f24d457c610a5ef8"  #r0.27.1
     tf_version = "v1.14.0"
 
     # Command line parser options
