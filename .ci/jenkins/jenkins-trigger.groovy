5440fb14a9a090d084dd61a691ac6682eff0032e
Krishnan Abhiram <kabhira@aberenbe-mobl1.amr.corp.intel.com>
2020-01-13 12:23:40 -0800

    Initial version of CI trigger script

---
+++ .ci/jenkins/jenkins-trigger.groovy
@@ -0,0 +1,19 @@
+String  PR_URL = CHANGE_URL
+String  PR_COMMIT_AUTHOR = CHANGE_AUTHOR
+String  PR_TARGET = CHANGE_TARGET
+String  JENKINS_BRANCH = "master"
+Integer TIMEOUTTIME = "7200"
+
+// Constants
+JENKINS_DIR = '.'
+
+timestamps {
+    node("trigger") {
+
+        deleteDir()  // Clear the workspace before starting
+        println("CI job completed")
+
+    }  // End:  node
+}  // End:  timestamps
+
+echo "Done"
