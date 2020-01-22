5ef5831a632c0045da3fa18157eed388dfea8498
Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>
2020-01-22 09:41:14 -0800

    Peg Pip to 19.3.1 for Macos target

--- test/ci/macos/run-ngraph-tf-mac-build.sh
+++ test/ci/macos/run-ngraph-tf-mac-build.sh
@@ -224,6 +224,7 @@ if [ -z "${tf_dir}" ] ; then
     # sudo --preserve-env --set-home pip install --ignore-installed ${PIP_INSTALL_EXTRA_ARGS:-} "${WHEEL_FILE}"
     # Here we are installing into a virtual environment, so DO NOT USE SUDO!!!
     pip install -U tensorflow==1.14.0
+    pip install -U pip==19.3.1
     set +x
 
 else 
