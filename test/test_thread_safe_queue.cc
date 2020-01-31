18dde1f6a2be38c9481fe6175210110ffceea330
Patil, Jitendra <jitendra.patil@intel.com>
2020-01-31 15:34:13 -0800

    format fixes

--- test/test_thread_safe_queue.cc
+++ test/test_thread_safe_queue.cc
@@ -23,8 +23,8 @@
 
 #include "gtest/gtest.h"
 #include "ngraph/runtime/chrome_trace.hpp"
-#include "ngraph_bridge/thread_safe_queue.h"
 #include "ngraph_bridge/ngraph_utils.h"
+#include "ngraph_bridge/thread_safe_queue.h"
 
 using namespace std;
 
@@ -52,18 +52,18 @@ TEST(ThreadSafeQueue, Simple) {
     while (item_count < 3) {
       {
         NG_TRACE("Consumer", "Do Wait", "");
-      while (consumer_do_wait) {
-        // cout << "\033[1;32mConsumer waiting\033[0m\n";
-        absl::SleepFor(absl::Milliseconds(1));
+        while (consumer_do_wait) {
+          // cout << "\033[1;32mConsumer waiting\033[0m\n";
+          absl::SleepFor(absl::Milliseconds(1));
+        }
       }
-      }
-      
+
       {
         NG_TRACE("Consumer", "Waiting", "");
-      consumer_state = WAITING_FOR_ITEM;
-      // cout << "\033[1;32mWaiting\033[0m" << endl;
-      queue.GetNextAvailable();
-}
+        consumer_state = WAITING_FOR_ITEM;
+        // cout << "\033[1;32mWaiting\033[0m" << endl;
+        queue.GetNextAvailable();
+      }
       // cout << "\033[1;32mGot Item: " << item_count << "\033[0m\n";
       item_count++;
       consumer_state = GOT_ITEM;
@@ -78,19 +78,19 @@ TEST(ThreadSafeQueue, Simple) {
   std::thread thread0(consumer);
   {
     NG_TRACE("Producer", "Waiting", "");
-  // Ensure that the consumer is in waiting state
-  ASSERT_TRUE(consumer_do_wait);
+    // Ensure that the consumer is in waiting state
+    ASSERT_TRUE(consumer_do_wait);
 
-  consumer_do_wait = false;
-  while (consumer_state != WAITING_FOR_ITEM) {
-    absl::SleepFor(absl::Milliseconds(1));
+    consumer_do_wait = false;
+    while (consumer_state != WAITING_FOR_ITEM) {
+      absl::SleepFor(absl::Milliseconds(1));
+    }
   }
-}
 
   // cout << "Now adding an item\n";
   {
     NG_TRACE("Producer", "Add", "");
-  queue.Add(nullptr);
+    queue.Add(nullptr);
   }
   // Wait until the consumer has a chance to move forward
   // cout << "Producer: Waiting for consumer to get ready" << endl;
@@ -105,9 +105,9 @@ TEST(ThreadSafeQueue, Simple) {
   // //cout << "Now adding two items\n";
   {
     NG_TRACE("Producer", "Add-2", "");
-  queue.Add(nullptr);
-  queue.Add(nullptr);
-}
+    queue.Add(nullptr);
+    queue.Add(nullptr);
+  }
 
   // cout << "Producer: Waiting for consumer to get ready" << endl;
 
