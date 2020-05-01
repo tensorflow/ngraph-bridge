04cf4b244cbfc3d5feec85de087f8fc3501ed32d
Kanvi Khanna <kanvi.khanna@intel.com>
2020-05-01 14:21:11 -0700

    missed changes

--- test_ngtf.py
+++ test_ngtf.py
@@ -99,7 +99,7 @@ def main():
 
     if (not os.path.isdir(build_dir + '/tensorflow')):
         download_repo(build_dir + "/tensorflow",
-                      "https://github.com/tensorflow/tensorflow.git", "v2.0.0")
+                      "https://github.com/tensorflow/tensorflow.git", "v1.15.2")
 
     # Next run the TensorFlow python tests
     os.environ['NGRAPH_TF_LOG_0_DISABLED'] = '1'
