3eb2f783c2e173b3963336881359e816def3e022
Abhishek Kulkarni <abhishek.kulkarni@intel.com>
2020-06-30 15:15:56 -0700

    Fix grappler build

--- ngraph_bridge/grappler/ngraph_optimizer.h
+++ ngraph_bridge/grappler/ngraph_optimizer.h
@@ -49,6 +49,8 @@ class NgraphOptimizer : public tensorflow::grappler::CustomGraphOptimizer {
 
   string name() const override { return "NgraphOptimizer"; };
 
+  bool UsesFunctionLibrary() const override { return true; }
+
   Status Init(
       const tensorflow::RewriterConfig_CustomGraphOptimizer* config) override;
 
