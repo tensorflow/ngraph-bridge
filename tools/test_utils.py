f1abde363b7e11819dc4a98651a2b7418ed90927
Abhishek Kulkarni <abhishek.kulkarni@intel.com>
2020-07-10 10:33:13 -0700

    Add a separate file for interpreter backend TF py tests

--- tools/test_utils.py
+++ tools/test_utils.py
@@ -277,9 +277,13 @@ def run_tensorflow_pytests_from_artifacts(backend, ngraph_tf_src_dir,
     # Now run the TensorFlow python tests
     test_src_dir = os.path.join(ngraph_tf_src_dir, "test/python/tensorflow")
     test_script = os.path.join(test_src_dir, "tf_unittest_runner.py")
-    if backend is not None and 'GPU' in backend:
-        test_manifest_file = os.path.join(test_src_dir,
-                                          "python_tests_list_gpu.txt")
+    if backend is not None:
+        if 'GPU' in backend:
+            test_manifest_file = os.path.join(test_src_dir,
+                                            "python_tests_list_gpu.txt")
+        elif 'INTERPRETER' in backend:
+            test_manifest_file = os.path.join(test_src_dir,
+                                            "python_tests_list_int.txt")
     else:
         if get_os_type() == 'Darwin':
             test_manifest_file = os.path.join(test_src_dir,
