e30601d8be5decfb391e4a13760194c7bda48ced
Kanvi Khanna <kanvi.khanna@intel.com>
2020-08-27 10:23:33 -0700

    possible fix for l2loss and remove commented code

--- ngraph_bridge/ngraph_builder.cc
+++ ngraph_bridge/ngraph_builder.cc
@@ -1829,16 +1829,15 @@ static Status TranslateL2LossOp(const Node* op,
   ng::Output<ng::Node> ng_input;
   TF_RETURN_IF_ERROR(GetInputNodes(ng_op_map, op, ng_input));
 
+  std::vector<float> val;
+  val.push_back(2.0);
   auto const_2 = ConstructNgNode<opset::Constant>(
-      op->name(), ng_input.get_element_type(), ng::Shape{},
-      std::vector<std::string>{"2"});
+      op->name(), ng_input.get_element_type(), ng::Shape{}, val[0]);
 
   auto ng_pow =
       ConstructNgNode<opset::Multiply>(op->name(), ng_input, ng_input);
 
   size_t input_rank = ng_input.get_shape().size();
-  // ng::AxisSet axes;
-  // std::vector<size_t> ng_reduction_axes_vect(axes.size());
   std::vector<int64> axes;
   for (size_t i = 0; i < input_rank; ++i) {
     axes.push_back(i);
