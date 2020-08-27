5b1913d7592bae32d5c3eb6d4f64affeda107c74
Banikumar Maiti (Intel AIPG) <banikumar.maiti@intel.com>
2020-08-26 19:44:40 -0700

    Parameterized GTests using TEST_P

--- test/test_math_ops.cpp
+++ test/test_math_ops.cpp
@@ -455,6 +455,84 @@ TEST_F(MathOpsSumFixture, FullSet) {
 }
 // END MathOpsSumFixture
 
+
+// BEGIN: parametrized Google Tests
+class MathOpsSum : public ::testing::TestWithParam<
+                       std::tuple<std::vector<int>, std::vector<int>, bool>> {
+ public:
+  static std::vector<std::tuple<std::vector<int>, std::vector<int>, bool>>
+  get_testing_params() {
+    std::vector<std::tuple<std::vector<int>, std::vector<int>, bool>>
+        testing_params;
+    vector<vector<int>> tshapes = {{2, 3},         {2, 2, 3},    {6, 2},
+                                   {2, 4, 3},      {4, 3, 2, 1}, {1, 2, 3, 4},
+                                   {3, 1, 2, 4, 1}};
+    vector<bool> v_keep_dims = {true, false};
+
+    for (auto tshp : tshapes) {
+      vector<vector<int>> vv_axis;
+      for (auto ax = 0; ax < tshp.size(); ax++) {
+        vv_axis.push_back({ax});
+      }
+      for (auto v_axis : vv_axis) {
+        for (auto keep_dims : v_keep_dims) {
+          testing_params.push_back(std::make_tuple(tshp, v_axis, keep_dims));
+        }
+      }
+    }
+    // Add few more tests with axes-combos
+    testing_params.push_back(
+        std::make_tuple(std::vector<int>{6, 2}, std::vector<int>{0, 1}, false));
+    testing_params.push_back(
+        std::make_tuple(std::vector<int>{6, 2}, std::vector<int>{0, 1}, true));
+    testing_params.push_back(std::make_tuple(std::vector<int>{2, 4, 3},
+                                             std::vector<int>{0, 1, 2}, false));
+
+    return testing_params;
+  }
+};
+
+TEST_P(MathOpsSum, SubTests) {
+  auto tshpvec = std::get<0>(GetParam());
+  auto axisvec = std::get<1>(GetParam());
+  auto keep_dims = std::get<2>(GetParam());
+  // TensorShape tshp({tshpvec.begin(), tshpvec.end()});
+  TensorShape tshp;
+  for (auto dim : tshpvec) {
+    tshp.AddDim(dim);
+  }
+
+  std::stringstream axisvec_ss;
+  std::copy(axisvec.begin(), axisvec.end(),
+            std::ostream_iterator<int>(axisvec_ss, ","));
+  NGRAPH_VLOG(5) << ">> Running parameterized sub-test"
+                 << ", tensor-shape=" << tshp << ", axis=" << axisvec_ss.str()
+                 << ", keep_dims=" << keep_dims;
+  Scope root = Scope::NewRootScope();
+  auto keep_dims_attr = ops::Sum::Attrs().KeepDims(keep_dims);
+
+  std::vector<int> vals = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6,
+                           1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6};
+  Tensor A(DT_INT32, TensorShape(tshp));
+  AssignInputValues<int>(A, vals);
+
+  Tensor B(DT_INT32, TensorShape({(int64)axisvec.size()}));
+  AssignInputValues<int>(B, axisvec);
+
+  auto R = ops::Sum(root, A, B, keep_dims_attr);
+  std::vector<Output> sess_run_fetchoutputs = {R};
+  OpExecuter opexecuter(root, "Sum", sess_run_fetchoutputs);
+  opexecuter.RunTest();
+}
+
+// Run like:
+// --gtest_filter="TestsWithParams*"
+// or, --gtest_filter="TestsWithParams/MathOpsSum.SubTests/0"
+INSTANTIATE_TEST_CASE_P(TestsWithParams, MathOpsSum,
+                        ::testing::ValuesIn(MathOpsSum::get_testing_params()));
+// END: parametrized Google Tests
+
+
 // Test op: Mean with & without keep dims & with both positive & negative axis
 TEST(MathOps, Mean) {
   int dim1 = 2;
