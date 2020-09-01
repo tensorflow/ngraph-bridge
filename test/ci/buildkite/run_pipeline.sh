685cf028ed2d0bf84d0665bad3c95746b23c1183
Banikumar Maiti (Intel AIPG) <banikumar.maiti@intel.com>
2020-08-31 23:14:58 -0700

    use automatic test manifest filename from OS, NGRAPH_TF_EXECUTOR & NGRAPH_TF_BACKEND

--- test/ci/buildkite/run_pipeline.sh
+++ test/ci/buildkite/run_pipeline.sh
@@ -6,6 +6,7 @@ echo "BUILDKITE_AGENT_META_DATA_QUEUE: ${BUILDKITE_AGENT_META_DATA_QUEUE}"
 echo "BUILDKITE_AGENT_META_DATA_NAME: ${BUILDKITE_AGENT_META_DATA_NAME}"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export NGRAPH_TF_EXECUTOR=ngraph
 
 # Always run setup for now
 PIPELINE_STEPS=" ${SCRIPT_DIR}/setup.yml "
@@ -15,6 +16,10 @@ if [ "${BUILDKITE_PIPELINE_NAME}" == "ngtf-cpu-ubuntu-grappler" ]; then
 elif [ "${BUILDKITE_PIPELINE_NAME}" == "ngtf-interpreter-ubuntu" ]; then
    export NGRAPH_TF_BACKEND=INTERPRETER
    PIPELINE_STEPS+=" ${SCRIPT_DIR}/ngtf-cpu-ubuntu.yml "
+elif [ "${BUILDKITE_PIPELINE_NAME}" == "ngtf-ie-ubuntu" ]; then
+   export NGRAPH_TF_EXECUTOR=IE
+   export NGRAPH_TF_BACKEND=CPU
+   PIPELINE_STEPS+=" ${SCRIPT_DIR}/ngtf-ie-ubuntu.yml "
 else
    PIPELINE_STEPS+=" ${SCRIPT_DIR}/${BUILDKITE_PIPELINE_NAME}.yml "
 fi
