40d919e442720e90e451659b5a662557bf402ff6
Banikumar Maiti (Intel AIPG) <banikumar.maiti@intel.com>
2020-10-07 11:33:27 -0700

    take artifacts path as argument to test/ci/buildkite/run_cppexample_infer.sh

--- test/ci/buildkite/run_cppexample_infer.sh
+++ test/ci/buildkite/run_cppexample_infer.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+PWD=`pwd`
+ARTIFACTS_DIR=$1
+if [ "${ARTIFACTS_DIR}" == "" ]; then
+    echo "Error: artifacts dir not specified!" && exit 1
+fi
 
-ret_code=1
+if [ ! -d "${ARTIFACTS_DIR}/examples" ]; then echo "Cannot find dir ${ARTIFACTS_DIR}/examples !"; exit 1; fi
+if [ ! -d "${ARTIFACTS_DIR}/lib" ]; then echo "Cannot find dir ${ARTIFACTS_DIR}/lib !"; exit 1; fi
 
 # Assumed pwd: /localdisk/buildkite/artifacts/$BUILDKITE_BUILD_ID/examples
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:../ngraph_bridge:../artifacts/lib"
+# or for local run .../build_cmake/artifacts/examples
 
-LOCALSTORE_PREFIX=/tmp
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ARTIFACTS_DIR}/lib:${ARTIFACTS_DIR}/tensorflow"
+echo "PWD=`pwd`, LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
 if [ "${BUILDKITE}" == "true" ]; then
     LOCALSTORE_PREFIX=/localdisk/buildkite/artifacts
+else
+    LOCALSTORE_PREFIX=/tmp
 fi
 LOCALSTORE=${LOCALSTORE_PREFIX}/pretrained_models
 if [ ! -d "${LOCALSTORE}" ]; then mkdir -p ${LOCALSTORE}; fi
@@ -39,6 +48,7 @@ LABELS=imagenet_slim_labels.txt
 #get_artifacts ${LABELS} "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
 get_artifacts ${LABELS} "https://www.dropbox.com/sh/racv0tcy60j49cf/AAA1GdoG1-oiSQfydBzvH833a/imagenet_slim_labels.txt?dl=0"
 
+cd ${ARTIFACTS_DIR}/examples
 ./cpp/inference/infer_single_network --graph=${MODEL} \
     --labels=${LABELS} \
     --image=${IMAGE} \
@@ -47,7 +57,8 @@ get_artifacts ${LABELS} "https://www.dropbox.com/sh/racv0tcy60j49cf/AAA1GdoG1-oi
 
 echo
 echo "Checking inference result..."
-grep 'peacock (85): 0.925' tmp_output && ret_code=0
+ret_code=1
+grep 'peacock (85): 0.925' tmp_output && echo "TEST PASSED" && ret_code=0
 rm tmp_output
 
 exit $((ret_code))
