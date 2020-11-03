04e305fd2de951b9e1782562c94276413457755d
kanvi-nervana <kanvi.khanna@intel.com>
2020-11-02 16:36:08 -0800

    Minor: update test name

--- test/python/test_maxpool3dgrad.py
+++ test/python/test_maxpool3dgrad.py
@@ -13,7 +13,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ==============================================================================
-"""nGraph TensorFlow bridge fusedConv2D tests.
+"""nGraph TensorFlow bridge Maxpool3DGrad tests.
 
 """
 from __future__ import absolute_import
